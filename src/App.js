0366b778f4dfee858657d51fe1881414dcdab7eb
Petr Majer <majer.petr@gmail.com>
2018-06-19 20:25:04 +0200

    Feat: Insert Map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import Map from './Map';
 
 class App extends Component {
+
+  componentWillMount() {
+    window.initMap = this.initMap;
+
+  }
+
+  initMap() {
+    let map = new window.google.maps.Map(document.querySelector('#map'), {
+      center: {"lat": 50.0516587, "lng": 14.4070306},
+      zoom: 13,
+      mapTypeControl: false
+    });
+
+  }
+
+  hambClick() {
+    const target = document.querySelector('aside')
+    target.classList.toggle('show')
+  }
+
   render() {
     return (
       <div className="App">
-
+        <header>
+          <button className='hamburger' onClick={() => this.hambClick()} >&#9776; </button>
+          <h1>  Mapa </h1>
+        </header>
+        <aside>
+          FILTER
+        </aside>
+        <main>
+          <Map />
+        </main>
       </div>
     );
   }
---
+++ src/Map.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Map extends Component {
+
+  componentWillMount() {
+    window.gm_authFailure = () => alert("Error, check API key");
+    const bodyTag = document.querySelector('body');
+    let mapsTag = document.createElement('script');
+    mapsTag.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCBr-4lNZOkFhaRjW_wkxMvvyxxBKpeDVQ&libraries=places,geometry,drawing&callback=initMap';
+    mapsTag.onerror = () => alert("Error between loading Google Maps");
+    mapsTag.async = true;
+    mapsTag.defer = true;
+    bodyTag.appendChild(mapsTag);
+  }
+
+  render() {
+    return (
+      <div id="map">Maps is loading....</div>
+    );
+  }
+}
+export default Map;
