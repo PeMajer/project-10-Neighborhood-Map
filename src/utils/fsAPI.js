71aa2b304d15e6aa8972040f2c29b2e59ab31268
Petr Majer <majer.petr@gmail.com>
2018-06-26 08:16:58 +0200

    Refactor: Edit error status

--- src/utils/fsAPI.js
+++ src/utils/fsAPI.js
@@ -7,20 +7,20 @@ export function fetchFourSquare (places) {
   const clientId = 'LL4UNDUA21U3BS4NRKJ1YZ3PI0ZRTNCOK4QW0E2STF51U0SX'
   const clientSecret = 'NUVCYT5FIODESPB0EB4GZ3BEZAM43GHXVJPX0HYYH0YJA4G1'
   const url = 'https://api.foursquare.com/v2/venues/'
-  const limit = 1
+
   const ver = '20180323'
 
   const self = this
 
   places.map(place => {
     const placeId = place.id
-    let urlRequest = `${url}${placeId}?client_id=${clientId}&client_secret=${clientSecret}&v=${ver}&limit=${limit}`
+    let urlRequest = `${url}${placeId}?client_id=${clientId}&client_secret=${clientSecret}&v=${ver}`
 
     fetch(urlRequest, {
         method: 'GET'
       }).then(function (response) {
         if (!response.ok) {
-          throw new Error(response.statusText ? response.statusText : 'Unknown error')
+          throw new Error(response.statusText ? response.statusText : response.status === 429 ? 'Daily call quota exceeded.' : 'Unknown error.' )
         }
         return response.json()
       }).then(function(data) {
